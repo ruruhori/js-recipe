97f74c490e485c7aa813631603a9d7624b6f9e9f
ruruhori <lulu.h000114@gmail.com>
2021-04-16 11:44:01 +0900

    レシピ1

--- 1_workspace/1/□と○/main.js
+++ 1_workspace/1/□と○/main.js
@@ -1,9 +1,5 @@
-const genkiFunction = function(number) {
-  for (let n = 1; n <= number; n++) {
-    if (n % 3 === 0) {
-      console.log(n + "!!!!!!!")
-    } else {
-      console.log(n)
-    }
-  }
+const figure = document.getElementById("figure")
+
+figure.onclick = function() {
+  figure.classList.toggle("rounded")
 }
