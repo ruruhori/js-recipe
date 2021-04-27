58628c4729a27beec15604472dc995352d8ca726
ruruhori <lulu.h000114@gmail.com>
2021-04-28 02:05:00 +0900

    電卓途中

---
+++ 3_semi/week3/main4.js
@@ -0,0 +1,28 @@
+const text = document.getElementById("text")
+const oneButton = document.getElementById("one-button")
+const twoButton = document.getElementById("two-button")
+const threeButton = document.getElementById("three-button")
+const fourButton = document.getElementById("four-button")
+const fiveButton = document.getElementById("five-button")
+const sixButton = document.getElementById("six-button")
+const sevenButton = document.getElementById("seven-button")
+const eightButton = document.getElementById("eight-button")
+const nineButton = document.getElementById("nine-button")
+const plusButton = document.getElementById("plus-button")
+const minusButton = document.getElementById("minus-button")
+const timesButton = document.getElementById("times-button")
+const dividedButton = document.getElementById("divided-button")
+const equalsButton = document.getElementById("equals-button")
+const clearButton = document.getElementById("clear-button")
+
+oneButton.onclick = function() {
+  display.textContent = 1
+}
+
+twoButton.onclick = function() {
+  display.textContent = 2
+}
+
+threeButton.onclick = function() {
+  display.textContent = 3
+}
