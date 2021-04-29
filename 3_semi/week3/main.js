e3b1fcf716624a06c3470d6c9bbb53d36cd4e984
ruruhori <lulu.h000114@gmail.com>
2021-04-29 17:59:11 +0900

    ３が大好き

--- 3_semi/week3/main.js
+++ 3_semi/week3/main.js
@@ -1,6 +1,6 @@
 const number = 100
 for (let n = 1; n <= number; n++) {
-  if ((n = srting.includes("3"))) {
+  if (srting(n).includes("3")) {
     console.log(`${n}!!!!!!!`)
   } else {
     console.log(n)
