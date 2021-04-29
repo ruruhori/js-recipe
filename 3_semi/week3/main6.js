65bc5fc43049c344825ee48596649844cf722524
ruruhori <lulu.h000114@gmail.com>
2021-04-29 18:55:47 +0900

    △わからない

--- 3_semi/week3/main6.js
+++ 3_semi/week3/main6.js
@@ -1,9 +1,5 @@
 const figure = document.getElementById("figure")
 
-// figure.onclick = function() {
-//   figure.classList.toggle("rounded")
-// }
-
 figure.onclick = function() {
   if (figure.classList.contains("transition square")) {
     figure.classList.remove("square")
@@ -15,4 +11,20 @@ figure.onclick = function() {
     figure.classList.remove("triangle")
     figure.classList.add("square")
   }
+  // figure.classList.toggle("rounded")
 }
+
+// figure.onclick = function() {
+// console.log(figure.classList)
+// if (figure.classList.contains("transition square")) {
+// figure.classList.remove("square")
+// figure.classList.add("rounded")
+// } else if (figure.classList.contains("transition rounded")) {
+// figure.classList.remove("square")
+// figure.classList.remove("rounded")
+// figure.classList.add("triangle")
+// } else if (figure.classList.contains("transition triangle")) {
+// figure.classList.remove("triangle")
+// figure.classList.add("square")
+// }
+// }
